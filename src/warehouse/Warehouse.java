5ea4a5e3dcc19d18b2ea32eb419f75f51526a26d
Diogo Silva <digax96@gmail.com>
2018-11-08 13:30:15 +0000

    Added location to warehouse

--- src/warehouse/Warehouse.java
+++ src/warehouse/Warehouse.java
@@ -1,11 +1,19 @@
 package warehouse;
 
+import java.awt.Point;
+
 import jade.core.Agent;
 
 public class Warehouse extends Agent {
 
-	public Warehouse() {
-		// TODO Auto-generated constructor stub
+	private Point location;
+	
+	public Warehouse(Point location) {
+		this.location = location;
+	}
+	
+	public Point getLocation() {
+		return location;
 	}
 
 }
