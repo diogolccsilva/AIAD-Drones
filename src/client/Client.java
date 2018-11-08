aeefc5d73e4a18bcf30d6190ac0fbdbd1d6a58bc
Diogo Silva <digax96@gmail.com>
2018-11-08 14:19:28 +0000

    Updated client

--- src/client/Client.java
+++ src/client/Client.java
@@ -12,24 +12,12 @@ public class Client extends Agent {
 
 	public void setup() {
 		System.out.println("Client created");
-		setAttributes(1, 1, "artigo");
 
 		//addBehaviour();
 	}
 
 	public void takeDown() {
 		System.out.println(getLocalName() + ": client killed");
-	}
-
-	public void setAttributes(Integer xPos, Integer yPos, String artc) {
-		addBehaviour(new OneShotBehaviour() {
-			public void action() {
-				xPosition = xPos;
-				yPosition = yPos;
-				articleName = artc;
-				System.out.println("Client lauched article " + articleName);
-			}
-		});
 	}
 	
 	public Point2D getLocation() {
