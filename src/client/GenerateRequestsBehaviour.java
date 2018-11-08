4209d591aa03edc2d2afb116e3e8524f70e6194d
Diogo Silva <digax96@gmail.com>
2018-11-08 16:02:28 +0000

    Updated generateRequests

--- src/client/GenerateRequestsBehaviour.java
+++ src/client/GenerateRequestsBehaviour.java
@@ -3,6 +3,10 @@ package client;
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+import deliveryPackage.DeliveryPackage;
+
 public class GenerateRequestsBehaviour extends TickerBehaviour {
 
 	public GenerateRequestsBehaviour(Agent a, long period) {
@@ -13,7 +17,14 @@ public class GenerateRequestsBehaviour extends TickerBehaviour {
 	@Override
 	protected void onTick() {
 		//randomly generate requests
-
+		boolean gen = ThreadLocalRandom.current().nextBoolean();
+		if (gen) {
+			Client sender = (Client)this.myAgent;
+			Client receiver = new Client(); //TODO: change this
+			double weight = 0;
+			double size = 0;
+			DeliveryPackage nPackage = new DeliveryPackage(sender,receiver,weight,size);
+		}
 	}
 
 }
