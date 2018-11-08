9435473fe91e3ccade9cc5bce26449535db7b8ef
Diogo Silva <digax96@gmail.com>
2018-11-08 17:21:58 +0000

    Behaviour now finds drones to send requests

--- src/client/GenerateRequestsBehaviour.java
+++ src/client/GenerateRequestsBehaviour.java
@@ -1,7 +1,12 @@
 package client;
 
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -13,20 +18,47 @@ public class GenerateRequestsBehaviour extends TickerBehaviour {
 		super(a, period);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
 	protected void onTick() {
-		//randomly generate requests
+		// randomly generate requests
 		boolean gen = ThreadLocalRandom.current().nextBoolean();
 		if (gen) {
-			Client sender = (Client)this.myAgent;
-			Client receiver = new Client(); //TODO: change this
+			Client sender = (Client) this.myAgent;
+			Client receiver = new Client(); // TODO: change this
 			double weight = 0;
 			double size = 0;
-			DeliveryPackage nPackage = new DeliveryPackage(sender,receiver,weight,size);
+			DeliveryPackage nPackage = new DeliveryPackage(sender, receiver, weight, size);
 			System.out.println(this.myAgent.getLocalName() + ": new request created");
+
+			AID[] drones = getDrones();
+			if (drones.length > 0) {
+				System.out.println(this.myAgent.getLocalName() + ": sending request to drones");
+			}
+			else {
+				System.out.println(this.myAgent.getLocalName() + ": no drones were found");
+			}
+			return;
 		}
-		System.out.println(this.myAgent.getLocalName() + ": not request was generated");
+		System.out.println(this.myAgent.getLocalName() + ": no request was generated");
 	}
 
+	public AID[] getDrones() {
+		AID[] drones = new AID[0];
+		DFAgentDescription template = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("delivery-service");
+		template.addServices(sd);
+		try {
+			DFAgentDescription[] result = DFService.search(myAgent, template);
+			System.out.println("Found the following drone agents:");
+			drones = new AID[result.length];
+			for (int i = 0; i < result.length; ++i) {
+				drones[i] = result[i].getName();
+			}
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		return drones;
+	}
 }
