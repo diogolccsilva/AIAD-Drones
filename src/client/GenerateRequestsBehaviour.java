1031f4456e2e5a41cf85fb9441d3061310dfc0c4
Diogo Silva <digax96@gmail.com>
2018-11-08 15:18:33 +0000

    Client behaviour to generate requests periodically

---
+++ src/client/GenerateRequestsBehaviour.java
@@ -0,0 +1,19 @@
+package client;
+
+import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+
+public class GenerateRequestsBehaviour extends TickerBehaviour {
+
+	public GenerateRequestsBehaviour(Agent a, long period) {
+		super(a, period);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void onTick() {
+		//randomly generate requests
+
+	}
+
+}
