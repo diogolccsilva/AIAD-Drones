1a5b73dc41e502c3950a0edabf44a73cc83e0a57
Alexandre <Alexandre@ssla.fe.up.pt>
2018-12-12 13:14:49 +0000

    user can now choose number of runs (pass as argument)

--- src/droneManagementSystem/Loop.java
+++ src/droneManagementSystem/Loop.java
@@ -10,7 +10,7 @@ public class Loop {
 	public static void main(String[] args) throws StaleProxyException, InterruptedException, IOException {
 		
 
-		for (int i=0; i<100; i++){
+		for (int i=0; i<Integer.parseInt(args[0]); i++){
 			Launcher launch = new Launcher();
 
 			System.out.println("Iteracao: "+i);
